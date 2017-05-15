a15b7f8a44285907cf0b99aed07cce61416b63b7
Dmytro Kalpakchi <dkalpackchi@gmail.com>
2017-05-15 21:51:10 +0200

    Split.py with cmdline arguments

--- split.py
+++ split.py
@@ -1,24 +1,35 @@
 #
 #	Splits the training data into four parts.
 #	dataset/train -> dataset/train[0-3]
+#	Example:
+#       python split.py path/to/train train
+#       parts_prefix - the basename for parts file, will be parts_prefix%d
+#
+
 import os
 
+import sys
+
+file_to_split = sys.argv[1]
+parts_prefix = sys.argv[2]
+
 file_num = 0
-path = 'dataset/train_parts/train%d' 
+path = 'dataset/train_parts/%s%d'
 if not os.path.exists(os.path.dirname(path)):
 	os.makedirs(os.path.dirname(path))
-writeFile = open(path % file_num, 'w')
+writeFile = open(path % (parts_prefix, file_num), 'w')
+
 lines_read = 0
 lines_in_file = 0
-with open('dataset/train') as fp:
+with open(file_to_split) as fp:
 	for line in fp:
 		if lines_in_file > 43000000:
 			record = line.split()
 			if record[1] == 'M':
-				print("%d lines in file%d " % (lines_in_file, file_num))
+				print("%d lines in %s%d " % (lines_in_file, parts_prefix, file_num))
 				writeFile.close()
 				file_num += 1
-				writeFile = open('dataset/train%d'%file_num, 'w')
+				writeFile = open(path%(parts_prefix, file_num), 'w')
 				lines_in_file = 0
 		writeFile.write(line)
 		lines_read += 1
