847afcfbe97690ac9752c92b71e18728c69bd40b
Henrik Karlsson <henakka@gmail.com>
2017-05-17 03:21:19 +0200

    Added features extraction for the new index

--- feature_extraction.py
+++ feature_extraction.py
@@ -14,6 +14,7 @@ import json
 import sys
 import time
 import logging
+import multiprocessing as mp
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 
@@ -21,61 +22,56 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 # Extract features from elasticsearch
 ##
 
-# Create an elasticsearch client
-es = Elasticsearch()
-es_index = 'yandex'
-#
-# step 1, get a serp
-#
 
-
-def get_session(day, size):
+def get_session(day):
     """
-    Get sessions from a specific day
+    Get sessions from a specific day, this function can take a while.
+    We should probably save the data from this into a file and then read from the file
     """
     query = \
         {
-            "size": size,
-            "_source": True,
+            "size": 0,
             "query": {
                 "match": {
                     "day": day
                 }
+            },
+            "aggs": {
+                "sessions": {
+                    "terms": {
+                        "field": "session",
+                        "size": 4000000
+                    }
+                }
             }
         }
-    page = es.search(index=es_index, doc_type='session',
-                     body=query, scroll="10m")
-    sid = page["_scroll_id"]
-    scroll_size = page['hits']['total']
-    yield page['hits']['hits']
-    while scroll_size > 0:
-        page = es.scroll(scroll_id=sid, scroll='10m')
-        sid = page['_scroll_id']
-        scroll_size = len(page['hits']['hits'])
-        yield page['hits']['hits']
+    page = es.search(index=es_index, doc_type='serp', body=query,
+                     filter_path=['**.key', '**.doc_count'])
+    # returns list of (session_id, number of serps in session)
+    return [(bucket['key'], bucket['doc_count']) for bucket in page['aggregations']['sessions']['buckets']]
 
 
-def get_serp(session):
+def get_serp(session_id):
     """
     Get serps for a batch of sessions
     """
     query = \
         {
-            "size": 10000,
+            "size": 300,
             "sort": [
                 {"serpId": "asc"}
             ],
             "query": {
-                "parent_id": {
-                    "type": "serp",
-                    "id": session["_id"]
+                "match": {
+                    "session": session_id
                 }
             }
         }
 
     res = es.search(index=es_index, doc_type='serp',
                     body=query, filter_path=['**._source'])
-    return res['hits']['hits']
+    serps = [r['_source'] for r in res['hits']['hits']]
+    return serps
 
 
 def get_features(serps):
@@ -93,11 +89,11 @@ def get_features(serps):
       9. Total displayed in same session same domain
       3. hit/miss/skip? (see dataiku sec 4.2.2)
     """
-    label_docs = serps[-1]["_source"]["documents"]
-    session_history = [serp["_source"] for serp in serps[:-1]]
+    label_docs = serps[-1]["documents"]
+    session_history = [serp for serp in serps[:-1]]
     num_features = 10
     features = np.zeros((10, num_features))
-    query = serps[-1]["_source"]["query"]
+    query = serps[-1]["query"]
 
     for pos, doc in enumerate(label_docs):
         site = doc['site']
@@ -110,7 +106,7 @@ def get_features(serps):
                 doc = serp["documents"][pos]
                 features[pos, 2] += doc["clicks"]
                 features[pos, 3] += doc["relevance"]
-                
+
             for doc in serp["documents"]:
                 if doc['site'] == site:
                     features[pos, 4] += doc['clicks']
@@ -120,7 +116,7 @@ def get_features(serps):
                     features[pos, 7] += doc['clicks']
                     features[pos, 8] += doc['relevance']
                     features[pos, 9] += 1
-                     
+
     return features
 
 
@@ -129,7 +125,7 @@ def get_labels(serp):
     Get the relevance labels from the serp
     """
     labels = []
-    for doc in serp['_source']['documents']:
+    for doc in serp['documents']:
         labels.append(doc['relevance'])
     return labels
 
@@ -150,11 +146,11 @@ def template_query():
 
     es.put_template(id="temp", body=body)
     res = es.search_template(index=es_index, body={
-            "inline": es.get_template(id="temp")["template"],
-            "params": {
-                "day": 5, 
-                "user": 10
-            }})
+        "inline": es.get_template(id="temp")["template"],
+        "params": {
+            "day": 5,
+            "user": 10
+        }})
     return res
 
 
@@ -163,7 +159,7 @@ def log_info(start_time, session_processed):
     elapsed = time.time() - start_time
     # lines per second
     sps = session_processed / elapsed
-    logging.info("Indexed %d lines, %.2f sps" % (session_processed, sps))
+    logging.info("Processed %d sessions, %.2f sps" % (session_processed, sps))
 
 
 """
@@ -172,7 +168,6 @@ Create dictionaries for session
 fill dictionaries with serp
 session -> serp
 """
-qid = 0
 
 if len(sys.argv) < 2:
     logging.error("""
@@ -186,23 +181,22 @@ day_range = sys.argv[1].split(':')
 start = int(day_range[0])
 end = start if len(day_range) == 1 else int(day_range[1])
 
+
+# Create an elasticsearch client
+es = Elasticsearch(timeout=3600)
+es_index = 'yandex'
 start_time = time.time()
 sessions_processed = 0
+qid = 0
 for day in range(start, end + 1):
-    for sessions in get_session(day, 100):
-        for session in sessions:
-            serps = get_serp(session)[:-1]
-            if len(serps) > 0:
-                features = get_features(serps)
-                labels = get_labels(serps[-1])
-                """ 
-                 print to RankLib file
-                """
-                dump2ranklib_file(qid, labels, features)
-                qid += 1
-            sessions_processed += 1
-            log_info(start_time, sessions_processed)
-
+    for (session_id, serp_count) in get_session(day):
+        if(serp_count < 3):
+            continue
+        serps = get_serp(session_id)[:-1]
+        features = get_features(serps)
+        labels = get_labels(serps[-1])
+        dump2ranklib_file(qid, labels, features)
+        qid += 1
 
 """
 days [1-24] -> history dataset
