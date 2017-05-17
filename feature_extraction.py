196bd59dfdb445e1ba53ae1351162597bdb6f469
Henrik Karlsson <henakka@gmail.com>
2017-05-17 11:43:18 +0200

    Modified feature extraction to follow new relevance labels

--- feature_extraction.py
+++ feature_extraction.py
@@ -14,7 +14,6 @@ import json
 import sys
 import time
 import logging
-import multiprocessing as mp
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 
@@ -45,10 +44,11 @@ def get_session(day):
                 }
             }
         }
-    page = es.search(index=es_index, doc_type='serp', body=query,
-                     filter_path=['**.key', '**.doc_count'])
+    buckets = es.search(index=es_index, doc_type='serp', body=query,
+                        filter_path=['**.key', '**.doc_count'])['aggregations']['sessions']['buckets']
+    
     # returns list of (session_id, number of serps in session)
-    return [(bucket['key'], bucket['doc_count']) for bucket in page['aggregations']['sessions']['buckets']]
+    return [(b['key'], b['doc_count']) for b in buckets]
 
 
 def get_serp(session_id):
@@ -79,14 +79,14 @@ def get_features(serps):
     Extracted features are:
       0. Unpersonalized rank
       1. Unpersonalized rank scaled with exp
-      2. Total number of clicks for the same query in same session
-      3. Total relevance of all clicks for the same query in same session
-      4. Total clicks in same session same site
-      5. Total relevance in same session same site
-      6. Total displayed in same session same site
-      7. Total clicks in same session same domain
-      8. Total relevance in same session same domain
-      9. Total displayed in same session same domain
+      2. Avg number of clicks for the same query in same session
+      3. Avg relevance of all clicks for the same query in same session
+      4. Avg clicks in same session same site
+      5. Avg relevance in same session same site
+      6. Avg displayed in same session same site
+      7. Avg clicks in same session same domain
+      8. Avg relevance in same session same domain
+      9. Avg displayed in same session same domain
       3. hit/miss/skip? (see dataiku sec 4.2.2)
     """
     label_docs = serps[-1]["documents"]
@@ -100,7 +100,7 @@ def get_features(serps):
         domain = doc['domain']
         features[pos, 0] = pos
         features[pos, 1] = np.exp(-pos)
-
+        
         for serp in session_history:
             if serp["query"] == query:
                 doc = serp["documents"][pos]
@@ -109,14 +109,14 @@ def get_features(serps):
 
             for doc in serp["documents"]:
                 if doc['site'] == site:
-                    features[pos, 4] += doc['clicks']
-                    features[pos, 5] += doc['relevance']
-                    features[pos, 6] += 1
+                    features[pos, 4] += doc['clicks'] / len(serps)
+                    features[pos, 5] += doc['relevance'] / len(serps)
+                    features[pos, 6] += 1 / len(serps)
                 if doc['domain'] == domain:
-                    features[pos, 7] += doc['clicks']
-                    features[pos, 8] += doc['relevance']
-                    features[pos, 9] += 1
-
+                    features[pos, 7] += doc['clicks'] / len(serps)
+                    features[pos, 8] += doc['relevance'] / len(serps)
+                    features[pos, 9] += 1 / len(serps)
+        
     return features
 
 
@@ -154,12 +154,12 @@ def template_query():
     return res
 
 
-def log_info(start_time, session_processed):
+def log_info(start_time, sessions_processed):
     # print indexing information
     elapsed = time.time() - start_time
     # lines per second
-    sps = session_processed / elapsed
-    logging.info("Processed %d sessions, %.2f sps" % (session_processed, sps))
+    sps = sessions_processed / elapsed
+    logging.info("Processed %d sessions, %.2f sps" % (sessions_processed, sps))
 
 
 """
@@ -185,19 +185,24 @@ end = start if len(day_range) == 1 else int(day_range[1])
 # Create an elasticsearch client
 es = Elasticsearch(timeout=3600)
 es_index = 'yandex'
-start_time = time.time()
 sessions_processed = 0
 qid = 0
+start_time = time.time()
 for day in range(start, end + 1):
-    for (session_id, serp_count) in get_session(day):
-        if(serp_count < 3):
-            continue
-        serps = get_serp(session_id)[:-1]
-        features = get_features(serps)
-        labels = get_labels(serps[-1])
-        dump2ranklib_file(qid, labels, features)
-        qid += 1
-
+    sessions = get_session(day)
+    sessions_processed = 0
+    start_time = time.time()
+    for (session_id, serp_count) in sessions:
+        if(serp_count > 1):
+            serps = get_serp(session_id)
+            labels = get_labels(serps[-1])
+            if np.sum(labels) == 0:
+                continue
+            features = get_features(serps)
+            dump2ranklib_file(qid, labels, features)
+            qid += 1
+        sessions_processed += 1
+        log_info(start_time, sessions_processed)
 """
 days [1-24] -> history dataset
 days [25-27] -> training set
